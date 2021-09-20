f67f89f98842826c05a1424d1df29aeaf5b61ac0
Kote_Animen <i.filyak@kvantron.com>
2021-09-20 13:50:01 +0300

    Добавьте файлы проекта.

---
+++ x_pract_4/x_pract_4/Page1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace x_pract_4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            frame1.IsEnabled = false;
+        }
+
+        private void rbt_Auto_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (rbt_Auto.IsChecked)
+            {
+                frame1.IsEnabled = true;
+            }
+        }
+    }
+}
---
+++ x_pract_4/x_pract_4/Page2.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace x_pract_4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Accept_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void rbt_Hand_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            
+        }
+    }
+}
